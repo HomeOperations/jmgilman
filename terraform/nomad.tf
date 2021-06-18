7ae8995d0a8a9de560efc487309241bf217b8488
Joshua Gilman <joshuagilman@gmail.com>
2021-06-17 22:09:35 -0700

    Adds provisioners for Nomad

--- terraform/nomad.tf
+++ terraform/nomad.tf
@@ -52,8 +52,40 @@ resource "vsphere_compute_cluster_vm_anti_affinity_rule" "nomad_servers_anti_aff
   virtual_machine_ids = values(module.nomad_servers)[*].uuid[0]
 }
 
+resource "null_resource" "nomad_servers_provisioner" {
+  depends_on = [
+    module.nomad_servers,
+  ]
+  provisioner "local-exec" {
+    command = "sleep 180"
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory/consul.vmware.yml ../ansible/consul_server.yml"
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory/nomad.vmware.yml ../ansible/nomad_server.yml"
+  }
+}
+
 resource "vsphere_compute_cluster_vm_anti_affinity_rule" "nomad_clients_anti_affinity_rule" {
   name                = "nomad_clients"
   compute_cluster_id  = "${data.vsphere_compute_cluster.cluster.id}"
   virtual_machine_ids = values(module.nomad_clients)[*].uuid[0]
+}
+
+resource "null_resource" "nomad_clients_provisioner" {
+  depends_on = [
+    module.nomad_servers,
+    module.nomad_clients,
+    null_resource.nomad_servers_provisioner,
+  ]
+  provisioner "local-exec" {
+    command = "sleep 180"
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory/consul.vmware.yml ../ansible/consul_client.yml"
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ../ansible/inventory/nomad.vmware.yml ../ansible/nomad_client.yml"
+  }
 }
